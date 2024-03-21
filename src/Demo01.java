93f7584193aecc4504c272bd93d12de5d1797aef
catr54 <1247144989@qq.com>
2024-03-21 07:19:32 +0000

    完成

--- src/Demo01.java
+++ src/Demo01.java
@@ -1,6 +1,9 @@
 public class Demo01 {
-    public static void main(){
-
+    public static void main(String[] args){
+        Person xiaoming = new Person();
+        xiaoming.setName("小明");
+        xiaoming.setAge(20);
+        System.out.println("名字：" + xiaoming.getName() + "+年龄：" + xiaoming.getAge());
     }
     
 }
@@ -9,5 +12,14 @@ class Person{
     private int age;
     public String getName(){
         return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public void setAge(int age){
+        this.age = age;
     }
 }
