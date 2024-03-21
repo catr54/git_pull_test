6b83606a5b3fbaecd7c61016e559cc60e576a417
catr54 <1247144989@qq.com>
2024-03-21 03:28:57 +0000

    321-1128

---
+++ src/Demo01.java
@@ -0,0 +1,13 @@
+public class Demo01 {
+    public static void main(){
+
+    }
+    
+}
+class Person{    
+    private String name;
+    private int age;
+    public String getName(){
+        return this.name;
+    }
+}
