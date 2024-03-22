f9be7ba2254a25b3f95edfe7cf10c32f6f52632c
catr54 <1247144989@qq.com>
2024-03-22 08:45:08 +0000

    tax计算（未完成）

--- src/Demo04.java
+++ src/Demo04.java
@@ -1,3 +1,4 @@
+//该文件定义了Student，Income,Salary,StateCouncilSpecialAllowance类
 public class Demo04 {
     public static void main(String[] args)
     {
@@ -9,6 +10,9 @@ public class Demo04 {
     //转型
     //向上转型（上级变量指向下级实例）,将子类->父类
         Person t = new Student();
+        t.run();//输出了student run
+        //即Java的实例方法调用是基于运行时的实际类型的动态调用，而非变量的声明类型。
+        //这就是多态，即针对某个类型的方法调用，其真正执行的方法取决于运行时期实际类型的方法。
     //向下转型需要判断某实例是否为某类型 变量名 instanceof 类型，返回布尔值
     //可以if，仅在true时转换
 
@@ -40,5 +44,39 @@ class Student extends Person{
     }
     public int getScore(){
         return score;
+    }
+}
+
+class Income{
+    protected double income;
+    public double getTax(){
+        return income * 0.1;
+    }
+    public Income(double income){
+        this.income = income;
+    }
+}
+
+class SalaryIncome extends Income{
+    public SalaryIncome(double income){
+        super(income);
+    }
+    @Override
+    public double getTax(){
+        if (income <= 5000) {
+            return 0;
+        } else {
+            return(income - 5000)* 0.2;        
+        }
+    }
+}
+
+class StateCouncilSpecialAllowance extends Income{
+    public StateCouncilSpecialAllowance(double income){
+        super(income);
+    }
+    @Override
+    public double getTax(){
+        return 0;
     }
 }
