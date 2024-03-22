0fd1cdd90afef4a5f7a13e5001ce1204305a8613
catr54 <1247144989@qq.com>
2024-03-22 03:19:04 +0000

    转型，继承与组合

--- src/Demo04.java
+++ src/Demo04.java
@@ -1,13 +1,20 @@
 public class Demo04 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Student xiaoli = new Student(20,"小李",80);
         System.out.println(xiaoli.getAge() + xiaoli.getName() + xiaoli.getScore());
         Student xiaodai = new Student();
         System.out.println(xiaodai.getAge() + xiaodai.getName() + xiaodai.getScore());
+    //转型
+    //向上转型（上级变量指向下级实例）,将子类->父类
+        Person t = new Student();
+    //向下转型需要判断某实例是否为某类型 变量名 instanceof 类型，返回布尔值
+    //可以if，仅在true时转换
 
     }
 }
 class Student extends Person{
+    //继承只可以是is关系，has关系应该用组合，即持有一个想要"继承"的实例
     private int score = 60;
     public Student(){
     }
