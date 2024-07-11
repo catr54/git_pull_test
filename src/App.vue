1366c78967a9abb047518d4e390bd2e6b550ab44
salt <1247144989@qq.com>
2024-07-11 16:06:10 +0800

    first changes

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    username<input type="text" v-model="username">
+  </div>
+  <div>
+    password<input type="password" v-model="password">
+  </div>
+  <div>
+    <button @click="login">login</button>
+  </div>
+</template>
+<script setup>
+  import { ref } from 'vue';
+  let username = ref('')
+  let password = ref('')
+  const login = ()=>{
+    console.log(username.value,password.value,'login successful')
+  }
+
+</script>
+<style scoped>
+
+</style>
---
+++ src/views/demo01.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        {{str}}
+    </div>
+</template>
+<script setup>
+
+let num  = 10;
+let str = 'hello';
+let add = (a)=>{
+    return a + num
+}
+</script>
+<style scoped>
+</style>
